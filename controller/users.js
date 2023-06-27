d815ea53ad2b1eeb58f141d1b27c45665e34b487
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2023-06-27 21:54:55 +0530

    rearranged in MVC Format

---
+++ controller/users.js
@@ -0,0 +1,113 @@
+const bcrypt = require('bcrypt')
+const userModel = require('../model/user.js')
+const jwt = require("jsonwebtoken")
+
+const salt = 10;
+const SECRET_KEY = "vkchauhangoldystirringminds"
+
+exports.getUsers = async (req, res) => {
+    try {
+        const allUsers = await userModel.find();
+        res.status(200).json({
+            status: "success",
+            allUsers
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "failed",
+            message: err.message
+        })
+    }
+}
+
+exports.registerUsers = async (req, res) => {
+    try {
+        const { email, password, cpassword } = req.body;
+
+        if (!email || !password || !cpassword) {
+            res.status(400).json({
+                status: "failed",
+                message: "Please enter all fields..!"
+            })
+        } else if (await userModel.findOne({ email })) {
+            res.status(400).json({
+                status: "failed",
+                message: "User is already registered..!"
+            })
+        } else if (password !== cpassword) {
+            res.status(400).json({
+                status: "failed",
+                message: "Password doesn't match..!"
+            })
+        } else {
+            bcrypt.hash(password, salt, async function (err, hashed) {
+                if (err) {
+                    res.status(400).json({
+                        status: 'failed',
+                        message: err.message
+                    })
+                } else {
+                    const newUser = await userModel.create({ email, password: hashed })
+                    res.status(200).json({
+                        status: "succes",
+                        message: "User registered successfully..!",
+                        newUser
+                    })
+                }
+            })
+        }
+    } catch (e) {
+        res.status(404).json({
+            status: "failed",
+            message: e.message
+        })
+    }
+}
+
+
+exports.loginUsers = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                status: "failed",
+                message: "All fields are mandatory...!"
+            })
+        }
+        const data = await userModel.findOne({ email })
+        if (!data) {
+            return res.status(400).json({
+                status: "failed",
+                message: "user is not registered...!"
+            })
+        }
+        bcrypt.compare(password, data.password, function (err, result) {
+            if (err) {
+                return res.status(400).json({
+                    status: "failed",
+                    message: err.message
+                })
+            }
+            if (result) {
+                const token = jwt.sign({ id: data._id }, SECRET_KEY)
+                return res.status(200).json({
+                    status: "success",
+                    message: "User loged in successfully....!",
+                    token
+                })
+            } else {
+                return res.status(400).json({
+                    status: "failed",
+                    message: "not a valid password"
+                })
+            }
+        })
+
+
+    } catch (err) {
+        return res.status(404).json({
+            status: "failed",
+            message: err.message
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express")
-const bodyparser = require('body-parser')
 const userRouter = require('./routes/user')
 const cors = require('cors')
 require('./database/connection')
 
-const app = express()
+
+const app = express();
 const port = 4500;
 app.use(cors());
-app.use(bodyparser.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.use('/user', userRouter);
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
+
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 })
